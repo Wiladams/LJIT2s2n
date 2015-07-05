6d74475a30b6c87b239c001a175b12baa4dcd864
William A Adams <spam@nanotechstyles.com>
2015-07-05 00:12:49 -0700

    Added MIT license

--- s2n.lua
+++ s2n.lua
@@ -1,9 +1,13 @@
 local ffi = require("ffi")
 local s2n_api = require("api.s2n_ffi")
-
+local S2NConnection = require("S2NConnection");
 
 local exports = {
-	S2NConnection = require("S2NConnection");
+	s2n_api = s2n_api;
+
+	-- classes
+	S2NConnection = S2NConnection;
 }
+
 
 return exports
